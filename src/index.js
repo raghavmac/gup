7582ca231c8be2acdb121ba4cd8243e39dd508d9
raghavmac <rghv.rocking@gmail.com>
2020-09-06 22:56:17 +0530

    Support for cli args as input

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const path = require('path');
 const Git = require('nodegit');
 const ora = require('ora');
+const arg = require('arg');
+const execa = require('execa');
 const { readdirSync } = require('fs');
 
 const spinner = ora('Loading');
 const currentDirectory = process.env.NODE_ENV === 'dev'
   ? path.resolve(process.cwd(), '../')
   : process.cwd();
+const args = arg({
+  '--nopull': Boolean,
+  '--find': String,
+  '-n': '--nopull',
+  '-f': '--find',
+});
 
 const directories = () => readdirSync(currentDirectory, { withFileTypes: true })
   .filter((directory) => directory.isDirectory())
@@ -34,14 +42,31 @@ async function open(directory) {
   }
 }
 
-async function execute() {
-  spinner.start();
-  await Promise
-    .all(directories().map(async (directory) => {
-      await open(directory);
-      spinner.succeed(directory);
-    }))
-    .finally(() => spinner.clear());
+async function git() {
+  if (args['--nopull']) {
+    spinner.info('No pull required!\n');
+  } else {
+    spinner.start();
+    await Promise
+      .all(directories().map(async (directory) => {
+        await open(directory);
+        spinner.succeed(directory);
+      }))
+      .finally(() => spinner.clear());
+  }
+}
+
+async function find() {
+  const query = args['--find'];
+  if (query) {
+    spinner.info(`Searching: ${query}\n`);
+    execa('rg', [query]).stdout.pipe(process.stdout);
+  }
+}
+
+function execute() {
+  git();
+  find();
 }
 
 execute();
